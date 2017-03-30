dec5f4b28c48e9262952e5ab1d78d80536525158
xianny <xianny@gmail.com>
2017-03-30 16:42:12 -0400

    load titanic data into app

--- build.sbt
+++ build.sbt
@@ -1,6 +1,8 @@
 name := "titanic"
 
 version := "1.0"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
+libraryDependencies ++= Seq(
+    "org.apache.spark" %% "spark-core" % "2.1.0",
+    "org.apache.spark" %% "spark-mllib" % "1.3.1")
 scalaVersion := "2.11.9"
     
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -4,17 +4,36 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.regression._
+import org.apache.spark.rdd._
+
+
+
 import java.io.File
 
 object SimpleApp {
+
   def main(args: Array[String]) {
-    val logFile = new File("").getAbsolutePath() + "/sample.txt"
-    val conf = new SparkConf().setAppName("Simple Application")
+
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+    val trainingFile = new File("").getAbsolutePath() + "/data/train.csv"
+    val testFile = new File("").getAbsolutePath() + "/data/test.csv"
+
+    // val categoricalFeaturesInfo = Map[Int, Int]()
+    // val impurity = "variance"
+    // val maxDepth = 5
+    // val maxBins = 32
+
+    
+    val csv: RDD[String] = sc.textFile(trainingFile).cache()
+
+    println("Done")
     sc.stop()
   }
+
 }
